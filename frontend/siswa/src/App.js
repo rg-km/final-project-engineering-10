cfdf37ae0d698e7225e5df439ea47681d5f67a64
friscaveronica08 <frisca.veronica08@gmail.com>
2022-06-19 20:55:11 +0700

    chore: add layout,register dan login

--- frontend/siswa/src/App.js
+++ frontend/siswa/src/App.js
@@ -1,12 +1,33 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/reusable/Layout";
 import "./App.css";
+import Login from "./components/pages/landing/Login";
+import Register from "./components/pages/landing/Register";
 
 function App() {
   return (
     <div className="">
       <BrowserRouter>
         <Routes>
-          
+          <Route path="/" element={<Layout type={"front"}></Layout>} />
+          <Route
+            path="/login"
+            element={
+              <Layout type={"front"}>
+                {" "}
+                <Login />{" "}
+              </Layout>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <Layout type={"front"}>
+                {" "}
+                <Register />
+              </Layout>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
