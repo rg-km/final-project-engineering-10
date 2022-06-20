97ccfe9e7828ad37de1097f2098cdab6aa5d6b89
Saifulloh Achmad Fajr <92873005+saifulAchmad@users.noreply.github.com>
2022-06-20 14:31:46 +0700

    update routes

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -12,17 +12,16 @@ func Routes(route *gin.Engine) {
 		v1.GET("/", repository.GetAll)
 		v1.POST("/login/", repository.Login)
 		v1.POST("/register/", repository.Register)
-		v1.GET("/list/", repository.GetAll)
 		v1.GET("/:id_siswa/", repository.GetUserById)
 		v1.POST("/enroll/",repository.AddMapel_siswa)
-		v1.DELETE("/enroll/delete/",repository.DeleteMapel_siswa)
+		v1.DELETE("/enroll/",repository.DeleteMapel_siswa)
 		v1.GET("/:id_siswa/mapel/search/", repository.SearchMapel)
-		v1.GET("/:id_siswa/mapel/list/", repository.GetAllMapel)
+		v1.GET("/:id_siswa/mapel/", repository.GetAllMapel)
 		v1.GET("/:id_siswa/mapel/:id_mapel/",repository.GetMapelByID)
 		v1.GET("/:id_siswa/mapel/:id_mapel/tugas/search/", repository.SearchTugas)
-		v1.GET("/:id_siswa/mapel/:id_mapel/tugas/list/", repository.GetAllTugas)
+		v1.GET("/:id_siswa/mapel/:id_mapel/tugas/", repository.GetAllTugas)
 		v1.GET("/:id_siswa/mapel/:id_mapel/tugas/:id_tugas/",repository.GetTugasById)
-		v1.POST("/:id_siswa/mapel/:id_mapel/tugas/:id_tugas/pengumpulan/create/",repository.SubmitTugas)
+		v1.POST("/:id_siswa/mapel/:id_mapel/tugas/:id_tugas/pengumpulan/",repository.SubmitTugas)
 		v1.GET("/:id_siswa/mapel/:id_mapel/tugas/:id_tugas/pengumpulan/:id_pengumpulan/",repository.GetPengumpulanTugasById)
 			
 	}
@@ -30,40 +29,25 @@ func Routes(route *gin.Engine) {
 
 	
 
-	// v2 := route.Group("/Guru")
-	// {
-	// 	v2.GET("/", repository.GetAllGuru)
-	// 	v2.POST("/login/", repository.GuruLogin)
-	// 	v2.POST("/register/", repository.RegisterGuru)
+	v2 := route.Group("/Guru")
+	{
+		v2.GET("/", repository.GetAllGuru)
+		v2.POST("/login/", repository.GuruLogin)
+		v2.POST("/register/", repository.RegisterGuru)
+		v2.POST("/:id_Guru/mapel/", repository.AddMapel)
+		v2.DELETE("/:id_Guru/mapel/:id/", repository.DeleteMapel)
+		v2.PUT("/:id_Guru/mapel/:id/", repository.UpdateMapel)
+		v2.GET("/:id_Guru/mapel/search/", repository.SearchMapel)
+		v2.GET("/:id_Guru/mapel/", repository.GetAllMapel)
+		v2.POST("/:id_Guru/mapel/list/:id_mapel/tugas/", repository.AddTugas)
+		v2.DELETE("/:id_Guru/mapel/list/:id_mapel/tugas/:id/", repository.DeleteTugas)
+		v2.PUT("/:id_Guru/mapel/list/:id_mapel/tugas/:id/", repository.UpdateTugas)
+		v2.GET("/:id_Guru/mapel/list/:id_mapel/tugas/search/", repository.SearchTugas)
+		v2.GET("/:id_Guru/mapel/list/:id_mapel/tugas/", repository.GetAllTugas)
+		v2.GET("/:id_Guru/mapel/list/:id_mapel/tugas/:id/",repository.GetTugasById)
+		v2.GET("/:id_Guru/mapel/list/:id_mapel/tugas/:id_tugas/pengumpulan/:id_pengumpulan/")
 
-	// 	v3 := route.Group("/:id_Guru/mapel")
-	// {
-	// 	v3.POST("/create/", repository.AddMapel)
-	// 	v3.DELETE("/:id/delete/", repository.DeleteMapel)
-	// 	v3.PUT("/:id/update/", repository.UpdateMapel)
-	// 	v3.GET("/search/", repository.SearchMapel)
-	// 	v3.GET("/list/", repository.GetAllMapel)
-
-	// 	v4 := route.Group(":id_mapel/tugas")
-	// 	{
-	// 		v4.POST("/create/", repository.AddTugas)
-	// 		v4.POST("/:id/delete/", repository.DeleteTugas)
-	// 		v4.POST("/:id/update/", repository.UpdateTugas)
-	// 		v4.GET("/search/", repository.SearchTugas)
-	// 		v4.GET("/list/", repository.GetAllTugas)
-	// 		v4.GET("/:id/",repository.GetTugasById)
-
-	// 		v5:= route.Group("/:id_/pengumpulan")
-	// 		{
-	// 			v5.GET("/list/")
-	// 			v5.GET("/:id/")
-	// 			v5.GET("/:id/nilai/")
-	// 		}
-	// 	}
-	
-	// }
-
-	// }
+	}
 
 
 }
