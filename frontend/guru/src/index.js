daa19c0a20f755d5f758adbf36f831439529ffcd
friscaveronica08 <frisca.veronica08@gmail.com>
2022-06-24 20:59:12 +0700

    chore: add landing page guru

--- frontend/guru/src/index.js
+++ frontend/guru/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import Prerequest from './components/HOC/Prerequest';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import './variables.css';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
