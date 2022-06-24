3196e809564232d520b3f614df917d88919a9888
Saifulloh Achmad Fajr <92873005+saifulAchmad@users.noreply.github.com>
2022-06-24 20:55:24 +0700

    chore : crud credit score

--- backend/repository/CreditScoreRepo.go
+++ backend/repository/CreditScoreRepo.go
@@ -17,10 +17,7 @@ func AddCreditScore(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	temp,err:=c.Cookie("user_id")
-	if err==http.ErrNoCookie {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	}
+	temp:=c.Param("id_siswa")
 	user_id,err:=strconv.Atoi(temp)
 	if err!=nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
@@ -63,5 +60,89 @@ func GetCreditScoreByUserId(c *gin.Context){
 
 }
 
+func GetCreditScoreByIdSiswa(c *gin.Context){
+	var credit models.Credit_score
+
+	if err:=c.ShouldBindJSON(&credit);err!=nil {
+		c.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
+		return
+	}
+
+	credits,err:=strconv.Atoi(credit.Id_siswa)
+	CheckErr(err)
+	user,err:=models.GetCreditScoreByIdSiswa(credits)
+	CheckErr(err)
+	if user.Id==0 {
+		c.JSON(http.StatusBadRequest,gin.H{"message": "Data Salah, Silahkan Masukkan ulang"})
+	}else{
+		c.JSON(http.StatusOK,gin.H{"data":user})
+	}
+
+}
+
+func GetCreditScoreById(c *gin.Context){
+	
+	var credit models.Credit_score
+
+	if err:=c.ShouldBindJSON(&credit);err!=nil {
+		c.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
+		return
+	}
+
+	// credits,err:=strconv.Atoi(credit.Id_siswa)
+	// CheckErr(err)
+	credits,err:= strconv.Atoi(c.Param("id_credit"))
+
+	user,err:=models.GetCreditScoreById(credits)
+	CheckErr(err)
+	if user.Id==0 {
+		c.JSON(http.StatusBadRequest,gin.H{"message": "Data Salah, Silahkan Masukkan ulang"})
+	}else{
+		c.JSON(http.StatusOK,gin.H{"data":user})
+	}
+
+}
 
 
+
+
+func UpdateStatusCredit(c *gin.Context){
+	var json models.Credit_score
+	id,err:=strconv.Atoi(c.Param("id_credit"))
+
+
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+
+	success, err := models.UpdateStatusCredit(json, id)
+
+	if success {
+		c.JSON(http.StatusOK, gin.H{"message": "Success"})
+		return
+	} else {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	}
+
+}
+
+func DeleteCredit (c *gin.Context){
+	credits, err := strconv.Atoi(c.Param("id_credit"))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid id credit"})
+	}
+
+	success, err := models.DeleteCredit(credits)
+
+	if success {
+		c.JSON(http.StatusOK, gin.H{"message": "Success"})
+	} else {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+	}	
+
+
+}
