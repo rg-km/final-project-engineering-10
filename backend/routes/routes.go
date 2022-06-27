2eb588979fccebb4329e259958668c8439fef36d
arhandev <arhandev.code@gmail.com>
2022-06-27 17:50:47 +0700

    fix:fix route

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -16,10 +16,10 @@ func Routes(route *gin.Engine) {
 		v1.GET("/:id_siswa/", repository.GetUserById)       //v
 		v1.POST("/enroll/", repository.AddMapel_siswa)      //v
 		v1.DELETE("/enroll/", repository.DeleteMapel_siswa) //vv
-		v1.GET("/avg", repository.AvgSekolah)
+		v1.GET("/avg/", repository.AvgSekolah)
 		v1.GET("/:id_siswa/avg/", repository.AvgByIdSiswa)
-		v1.GET("/:id_siswa/avg/:id_mapel/", repository.AvgByMapel)
-		v1.GET("/:id_siswa/avg/:id_mapel/me", repository.AvgByMapelAndSiswa)
+		v1.GET("/avg/mapel/:id_mapel/", repository.AvgByMapel)
+		v1.GET("/:id_siswa/avg/:id_mapel/me/", repository.AvgByMapelAndSiswa)
 		v1.GET("/:id_siswa/credit/", repository.GetCreditScoreByIdSiswa)
 		v1.GET("/:id_siswa/credit/:id_credit/", repository.GetCreditScoreById)
 		v1.GET("/:id_siswa/mapel/search/", repository.SearchMapel)
@@ -77,7 +77,6 @@ func Routes(route *gin.Engine) {
 		v2.GET("/:id_Guru/minat/:id_minat/", repository.GetMinatById)
 		v2.PUT("/:id_Guru/minat/:id_minat/", repository.UpdateMinat)
 		v2.DELETE("/:id_Guru/minat/:id_minat/", repository.DeleteMinat)
-
 	}
 
 }
--- frontend/guru/src/components/pages/dashboard/Dashboard.jsx
+++ frontend/guru/src/components/pages/dashboard/Dashboard.jsx
@@ -1,105 +1,49 @@
-import { Tag } from 'antd';
-import React from 'react';
-
-const columns = [
-	{
-		title: 'No',
-		dataIndex: 'no',
-		key: 'no',
-		render: (item, record, index) => <>{index + 1}</>,
-		className: 'text-center',
-	},
-	{
-		title: 'Nama Tugas',
-		dataIndex: 'nama_tugas',
-		key: 'nama_tugas',
-		className: 'text-center',
-	},
-	{
-		title: 'Nilai Tugas',
-		dataIndex: 'nilai_tugas',
-		key: 'nilai_tugas',
-		className: 'text-center',
-	},
-	{
-		className: 'text-center',
-		title: 'Status',
-		key: 'status',
-		dataIndex: 'status',
-		render: status => {
-			let color;
-			let caption;
-
-			if (status === 'belum') {
-				color = '#F9B577';
-				caption = 'Belum Mengumpulkan';
-			} else if (status === 'review') {
-				color = '#2F71EB';
-				caption = 'Sedang dalam penilaian';
-			} else if (status === 'lulus') {
-				color = '#A1FF80';
-				caption = 'Lulus';
-			} else {
-				color = '#FC3E32';
-				caption = 'Tidak Lulus';
-			}
-
-			return (
-				<div className="flex justify-center ">
-					<Tag className="text-lg" color={color}>
-						{caption}
-					</Tag>
-				</div>
-			);
-		},
-	},
-	{
-		className: 'text-center',
-		title: 'Action',
-		key: 'action',
-		render: (_, record) => (
-			<div>
-				<img src="/image/dashboard/file.svg" alt="edit" />
-			</div>
-		),
-	},
-];
-
-const data = [
-	{
-		key: '1',
-		nama_tugas: 'Aljabar',
-		nilai_tugas: '-',
-		status: 'belum',
-	},
-	{
-		key: '2',
-		nama_tugas: 'Perkalian',
-		nilai_tugas: '-',
-		status: 'review',
-	},
-	{
-		key: '3',
-		nama_tugas: 'Pertambahan',
-		nilai_tugas: 95,
-		status: 'lulus',
-	},
-	{
-		key: '4',
-		nama_tugas: 'Pembagian',
-		nilai_tugas: 40,
-		status: 'tidak_lulus',
-	},
-	{
-		key: '5',
-		nama_tugas: 'Pertambahan',
-		nilai_tugas: 95,
-		status: 'lulus',
-	},
-];
-
 function Dashboard() {
-	return <div></div>;
+	return (
+		<div className="">
+			<h1 className="text-3xl font-bold">Dashboard</h1>
+			<div className="flex gap-10 justify-center">
+				<div>
+					<h2 className="text-center text-2xl font-bold my-5">Profil Sekolah</h2>
+					<div className="bg-[#FF985E] bg-opacity-80 w-full rounded-2xl p-4 flex flex-col items-center justify-center">
+						<div>
+							<img src="/image/dashboard/building.svg" className="w-40" alt="" />
+						</div>
+						<div className="my-4 grid grid-cols-7 text-lg font-bold gap-3 justify-center items-center text-center">
+							<div className="py-3 px-2 bg-white rounded-lg col-span-3">Nama Sekolah</div>
+							<div className="py-3 px-2 bg-white rounded-lg col-span-1">:</div>
+							<div className="py-3 px-2 bg-white rounded-lg col-span-3">SMAN 08 Pekanbaru</div>
+							<div className="py-3 px-2 bg-white rounded-lg col-span-3">Kepala Sekolah</div>
+							<div className="py-3 px-2 bg-white rounded-lg col-span-1">:</div>
+							<div className="py-3 px-2 bg-white rounded-lg col-span-3">Hehe.Msc</div>
+							<div className="py-3 px-2 bg-white rounded-lg col-span-3">Kode Sekolah</div>
+							<div className="py-3 px-2 bg-white rounded-lg col-span-1">:</div>
+							<div className="py-3 px-2 bg-white rounded-lg col-span-3">SMAN 08 Pekanbaru</div>
+						</div>
+					</div>
+				</div>
+				<div>
+					<h2 className="text-center text-2xl font-bold my-5">Profil Guru</h2>
+					<div className="bg-[#9747FF] bg-opacity-80 w-full rounded-2xl p-4 flex flex-col items-center justify-center">
+						<div>
+							<img src="/image/dashboard/profile.svg" className="w-40" alt="" />
+						</div>
+						<div className="my-4 grid grid-cols-7 text-lg font-bold gap-3 justify-center items-center text-center">
+							<div className="py-3 px-2 bg-white rounded-lg col-span-3">Nama Guru</div>
+							<div className="py-3 px-2 bg-white rounded-lg col-span-1">:</div>
+							<div className="py-3 px-2 bg-white rounded-lg col-span-3">Farhan</div>
+							<div className="py-3 px-2 bg-white rounded-lg col-span-3">Email</div>
+							<div className="py-3 px-2 bg-white rounded-lg col-span-1">:</div>
+							<div className="py-3 px-2 bg-white rounded-lg col-span-3 overflow-auto">arhandev.code@gmail.com</div>
+							<div className="py-3 px-2 bg-white rounded-lg col-span-3">Kode Sekolah</div>
+							<div className="py-3 px-2 bg-white rounded-lg col-span-1">:</div>
+							<div className="py-3 px-2 bg-white rounded-lg col-span-3">SMAN 08 Pekanbaru</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Dashboard;
