0a75e9a552253dbe92cef96ef706b8e6ef963f06
arhandev <arhandev.code@gmail.com>
2022-06-27 21:26:55 +0700

    fix: add multiple cors and remove custom cors

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
-	"mactiv/middleware"
+	// "mactiv/middleware"
 	"mactiv/models"
 	"mactiv/repository"
 	"mactiv/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.Use(middleware.CORSMiddleware())
+	// r.Use(middleware.CORSMiddleware())
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000", "http://localhost:3001"}
+	config.AllowHeaders = []string{"Content-Type", "Content-Length", "accept", "Authorization", "X-CSRF-Token", "Accept-Encoding", "Cache-Control", "origin"}
+	config.AllowMethods = []string{"POST", "HEAD", "PATCH", "OPTIONS", "GET", "PUT", "DELETE"}
+	config.AllowCredentials = true
+	r.Use(cors.New(config))
 	routes.Routes(r)
 	err := models.ConnectDB()
 	repository.CheckErr(err)
